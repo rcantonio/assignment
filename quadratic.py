bc483965f94ff95adb692a83cb378840cd49cd16
rcantonio <rcantoniojr@gmail.com>
2016-09-10 10:50:59 -0400

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -8,6 +8,13 @@ b = int(input("What is the value of B? "))
 c = int(input("What is the value of C? "))
 x = int(input("What is the value of X? "))
 
+
+'''
+So the following lines of code are only meant to prevent
+the program from outputting "+-" on the quadratic equation
+when the user inputs a negative number.
+'''
+
 quadratic="The following quadratic was entered: " + str(a) + "X^2"
 if ( b >= 0):
     quadratic += ("+" + str(b) + "X")
@@ -17,4 +24,6 @@ if (c >= 0):
     quadratic +=  ("+" + str(c))
 else:
     quadratic +=  (str(c))
-print (quadratic + "\nThe value of the quadratic is: " + str((a*(x*x)) + (b*x) + c))
+
+print (quadratic)
+print("The value of the quadratic is: " + str((a*(x*x)) + (b*x) + c))
