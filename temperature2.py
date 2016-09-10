d4224f8586c9a6ae457ccbc8cb9dbd3a109f671e
rcantonio <rcantoniojr@gmail.com>
2016-09-10 10:50:07 -0400

    Update temperature2.py

--- temperature2.py
+++ temperature2.py
@@ -6,4 +6,4 @@ def toFahrenheit (celsius):
     return celsius * 1.8 + 32
 
 for a in range (0, 101, 5):
-    print (str(a) + " celsius = " + str(toFahrenheit(a)) + " fahrenheit") 
+    print ("", a, "celsius\t<--->\t", toFahrenheit(a), "fahrenheit") 
