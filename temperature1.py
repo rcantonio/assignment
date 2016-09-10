aa39c8ac5108231315d322f561bb3deddd02fa71
rcantonio <rcantoniojr@gmail.com>
2016-09-10 10:49:44 -0400

    Update temperature1.py

--- temperature1.py
+++ temperature1.py
@@ -2,11 +2,11 @@
 Assignment 1: Temperature
 '''
 
-again = True
-while again:
-    temperature = int(input("Enter temperature in fahrenheit: "))
-    celsius = (temperature - 32) * (5/9)
-    print(str(temperature) + " fahrenheit = " + str(celsius) + " celsius")
+keepAsking = True
+while keepAsking:
+    fahrenheit = int(input("Enter temperature in fahrenheit: "))
+    celsius = (fahrenheit - 32) * (5/9)
+    print(fahrenheit, "fahrenheit =", celsius, "celsius")
     repeat = input("Do you want to convert another temperature? [y/n]: ")
-    if (repeat != "Y" and repeat != "y"):
+    if (repeat.lower() != "y"):
         again = False
